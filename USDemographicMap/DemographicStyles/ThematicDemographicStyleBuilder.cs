3bdba624f835b894954dac055c1660181505394a
Mike-ThinkGeo <mikeyu@thinkgeo.com>
2017-01-13 17:36:29 +0800

    Check out to branch FixObsoleteStylesWarning.

--- USDemographicMap/DemographicStyles/ThematicDemographicStyleBuilder.cs
+++ USDemographicMap/DemographicStyles/ThematicDemographicStyleBuilder.cs
@@ -80,7 +80,6 @@ namespace ThinkGeo.MapSuite.USDemographicMap
                 classBreakStyle.ClassBreaks.Add(classBreak);
             }
 
-            //return new Collection<Style>() { classBreakStyle, TextStyles.Country1("NAME") };
             return new Collection<Style>() { classBreakStyle, TextStyles.CreateSimpleTextStyle("NAME", "Arial", 8, DrawingFontStyles.Bold, GeoColor.StandardColors.Black, GeoColor.StandardColors.White, 3) };
         }
 
